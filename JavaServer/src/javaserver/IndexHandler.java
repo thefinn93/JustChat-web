30f842276d66c990c360e4779eade29799f61710
minogb <minoguebrad@rocketmail.com>
2015-05-12 18:38:39 -0700

    Added handler for "/"

---
+++ JavaServer/src/javaserver/IndexHandler.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaserver;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+
+/**
+ *
+ * @author FryedMan
+ */
+public class IndexHandler  implements HttpHandler{
+
+    @Override
+    public void handle(HttpExchange he) throws IOException {
+        String response = "What?";
+        he.sendResponseHeaders(200, response.length());
+        OutputStream oout = he.getResponseBody();
+        oout.write(response.getBytes());
+        oout.close();
+    }
+    
+}
