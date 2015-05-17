469c7ba76327e6b0e3614b2b2a378bd127ef435e
minogb <minoguebrad@rocketmail.com>
2015-05-16 23:24:02 -0700

    accumulated constant strings, no configurations here

---
+++ JavaServer/src/javaserver/Definitions.java
@@ -0,0 +1,22 @@
+
+package javaserver;
+
+/**
+ *
+ * @author Brad Minogue
+ */
+public class Definitions {
+    public static final String WAKEUP_MESSAGE = "Good Morning";
+    public static final String GOOD_STARTUP_MESSAGE = "System Started Successfully";
+    public static final String ENDL = "<br>";
+    public static final String S_HTML = "<html>";
+    public static final String E_HTML = "</html>";
+    public static final String SSL_CLIENT_VERIFY = "Client-Verify";
+    public static final String SSL_CLIENT_CERT = "Client-Certificate";
+    public static final String SSL_CLIENT_FINGERPRINT = "Client-Certificate-fp";
+    public static final String SSL_CLIENT_SERIAL = "Client-Serial";
+    public static final String SSL_CLIENT_S_DN = "Client-S-DN";
+    public static final String SSL_CLIENT_I_DN = "Client-I-DN";
+    public static final String SSL_Cipher = "SSL-Cipher";
+    public static final String FOWARD_FOR = "X-Forward-For";
+}
