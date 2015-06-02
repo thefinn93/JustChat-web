6adeba766069b2f8187b6c48d90e2108c356957b
Finn <finn@finn.io>
2015-06-01 18:40:49 -0700

    Created a simple CA program based on the original main.py

---
+++ CA/main.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+from flask import Flask, request, jsonify, make_response
+import subprocess
+import random
+from raven.contrib.flask import Sentry
+app = Flask(__name__)
+
+# Sentry settings
+dsn = "https://9b87b67e2eb64cfe8b1e25b6fe91622c:481dfdff04fb423d9ea84e878de4945a@sentry.finn.io/4"
+
+
+# Headers
+ssl_client_verify = "Client-Verify"
+ssl_client_cert = "Client-Certificate"
+ssl_client_fingerprint = "Client-Certificate-fp"
+ssl_client_serial = "Client-Serial"
+ssl_client_s_dn = "Client-S-DN"
+ssl_client_i_dn = "Client-I-DN"
+ssl_cipher = "SSL-Cipher"
+forwarded_for = "X-Forwarded-For"
+
+
+# Raven Client
+app.config['SENTRY_DSN'] = dsn
+sentry = Sentry(app)
+
+cacrt = "/etc/ssl/ca/ca.crt"
+cakey = "/etc/ssl/ca/ca.key"
+opensslcmd = ['openssl', 'ca', '-keyfile', cakey, '-cert', cacrt,
+              '-extensions', 'usr_cert', '-notext', '-md', 'sha256', '-spkac',
+              '/dev/stdin', '-out', '/dev/stdout']
+
+removedchars = [
+    "\n",
+    "\r",
+    "\0",
+    "\x0B"
+]
+
+certAttributes = {
+    "countryName": "US",
+    "stateOrProvinceName": "Washington",
+    "localityName": "Bothell",
+    "organizationName": "JustChat Enterprises",
+    "0.OU": "whut",
+    "CN": "lel"
+}
+
+
+@app.route("/keysign", methods=["POST"])
+def keysign():
+    response = make_response("Whoops, no pubkey specified!")
+    if ssl_client_verify in request.headers:
+        if "pubkey" in request.form and request.headers[ssl_client_verify] == "NONE":
+            pubkey = "SPKAC="
+            spkac = request.form['pubkey']
+            for char in removedchars:
+                spkac = spkac.replace(char, "")
+            pubkey += spkac
+
+            certAttributes['CN'] = random.randint(0, 1000)
+            if "CN" in request.form:
+                certAttributes['CN'] = request.form['CN']
+
+            for attribute in certAttributes:
+                pubkey += "\n%s=%s" % (attribute, certAttributes[attribute])
+            try:
+                openssl = subprocess.check_output(opensslcmd,
+                                                  input=pubkey.encode('utf-8'))
+                response = make_response(openssl)
+                response.headers['Content-Type'] = "application/x-x509-user-cert"
+            except subprocess.CalledProcessError:
+                response = jsonify({
+                    "result": "OpenSSL command failed",
+                    "command": opensslcmd,
+                    "stdin": pubkey
+                })
+        else:
+            return "You silly goose, you already have a certificate! You can't make another one"
+    return response
+
+#
+# @app.route('/sign')
+# def signCSR():
+#     response = {
+#         "result": "failure",
+#         "reason": "Unknown"
+#     }
+#     try:
+#         if ssl_client_verify in request.headers:
+#             if "csr" in request.form and request.headers[ssl_client_verify] == "SUCCESS":
+#                 with open(caInfoFile, 'w') as caInfo:
+#                     csr = crypto.load_certificate_request(crypto.TYPE_RSA, request.form['csr'])
+#
+#                     info = json.load(caInfo)
+#                     certinfo = {
+#                         "serial": info['certs'][-1]['serial'] + 1,
+#                         "cn": csr.getSubject()
+#                     }
+#
+#                     cert = crypto.X509()
+#                     cert.set_serial_number(certinfo['serial'])
+#                     cert.gmtime_adj_notBefore(0)
+#                     cert.gmtime_adj_notAfter(60*60*24*365*5)
+#                     cert.set_issuer(issuer)
+#                     cert.set_subject(csr.getSubject())
+#                     cert.set_pubkey(csr.get_pubkey(), "sha256")
+#                     cert.sign(ca.get_privatekey(), "sha256")
+#     except crypto.Error as e:
+#         sentry.captureException()
+#         response['reason'] = "Exception"
+#         response['exception'] = str(e)
+#
+#     return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
